fe7dd3801af5bb4e6bc6bb798229047b5823ffc8
Adetunji <ashennaike@gmail.com>
2019-05-17 13:00:55 -0400

    finished seed and migration

--- data/migrations/20190517124501_create_gamelist_table.js
+++ data/migrations/20190517124501_create_gamelist_table.js
@@ -1,8 +1,25 @@
 
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('games', tbl => {
+    tbl.increments();
+
+    tbl
+    .string('Title', 128)
+    .notNullable()
+    .unique();
+
+    tbl
+    .integer('Year')
+    .notNullable()
+
+    tbl
+    .string('System', 128)
+    .notNullable()
+
+  })
   
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('games');
 };
